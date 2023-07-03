575812bd1c58f2c9faa1ea035f13be0bb7f71e93
Edvard Kunst <kunstedvard@mail.ru>
2023-07-03 15:51:56 +0300

    pull commit

---
+++ tests/test_registration.py
@@ -0,0 +1,23 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from locators import Locators as L
+from faker import Faker
+
+faker = Faker()
+def test_registr_user(driver):
+    driver.get("https://stellarburgers.nomoreparties.site/register")
+    driver.find_element(*L.NAME_INPUT).send_keys(faker.name())
+    driver.find_element(*L.MAIL_INPUT).send_keys(faker.email())
+    driver.find_element(*L.PASSWORD_INPUT).send_keys("qwerty12345")
+    driver.find_element(*L.REGISTER_BUTTON).click()
+    WebDriverWait(driver,5).until(EC.visibility_of_element_located(L.LOGIN_BUTTON))
+    assert driver.find_element(*L.LOGIN_BUTTON).text == 'Войти'
+
+def test_registr_password_error(driver):
+    driver.get("https://stellarburgers.nomoreparties.site/register")
+    driver.find_element(*L.NAME_INPUT).send_keys(faker.name())
+    driver.find_element(*L.MAIL_INPUT).send_keys(faker.email())
+    driver.find_element(*L.PASSWORD_INPUT).send_keys("12345")
+    driver.find_element(*L.REGISTER_BUTTON).click()
+    WebDriverWait(driver, 5).until(EC.visibility_of_element_located(L.REGISTER_PASSWORD_ERROR))
+    assert driver.find_element(*L.REGISTER_PASSWORD_ERROR).text == "Некорректный пароль"
---
+++ tests/test_switch_pages.py
@@ -0,0 +1,36 @@
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from locators import Locators as L
+
+def test_switch_profile_button(driver):
+    driver.get("https://stellarburgers.nomoreparties.site/")
+    driver.find_element(*L.PROFILE_BUTTON).click()
+    WebDriverWait(driver, 5).until(EC.visibility_of_element_located(L.LOGIN_BUTTON))
+    assert driver.find_element(*L.LOGIN_BUTTON).text == "Войти"
+
+def test_switch_profile_to_constructor(driver):
+    driver.get("https://stellarburgers.nomoreparties.site/")
+    driver.find_element(*L.PROFILE_BUTTON).click()
+    driver.find_element(*L.CONSTRUCTOR_BUTTON).click()
+    WebDriverWait(driver, 5).until(EC.visibility_of_element_located(L.CREATE_BURGER))
+    assert driver.find_element(*L.CREATE_BURGER).text == "Соберите бургер"
+
+def test_switch_to_logo(driver):
+    driver.get("https://stellarburgers.nomoreparties.site/login")
+    driver.find_element(*L.LOGO_BUTTON).click()
+    WebDriverWait(driver, 5).until(EC.visibility_of_element_located(L.CREATE_BURGER))
+    assert driver.find_element(*L.CREATE_BURGER).text == "Соберите бургер"
+
+def test_logout_button(driver):
+    driver.get("https://stellarburgers.nomoreparties.site/login")
+    driver.find_element(*L.MAIL_INPUT).send_keys("kunst_edvard1996@mail.ru")
+    driver.find_element(*L.PASSWORD_INPUT).send_keys("qwerty123")
+    driver.find_element(*L.LOGIN_BUTTON).click()
+    WebDriverWait(driver, 5).until(EC.visibility_of_element_located(L.PROFILE_BUTTON))
+    driver.find_element(*L.PROFILE_BUTTON).click()
+    WebDriverWait(driver, 5).until(EC.visibility_of_element_located(L.LOGOUT_BUTTON))
+    driver.find_element(*L.LOGOUT_BUTTON).click()
+    WebDriverWait(driver, 5).until(EC.visibility_of_element_located(L.LOGIN_BUTTON))
+    assert driver.find_element(*L.LOGIN_BUTTON).text == "Войти"
+
+
